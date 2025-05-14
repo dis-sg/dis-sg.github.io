d5a6756e91c94654d46dd762abe904cb5e10764a
dis-sg <56503652+dis-sg@users.noreply.github.com>
2025-05-14 21:43:56 +0800

    Update lunrsearchengine.js

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -19,7 +19,22 @@ var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or p
     "url": "{{ site.url }}{{site.baseurl}}{{ page.url }}",
     "title": "{{ page.title }}",
     "body": "{{ page.date | date: "%Y/%m/%d" }} - {{ page.content | markdownify | replace: '.', '. ' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"{% assign counter = counter | plus: 1 %}
-    }{% if forloop.last %}{% else %}, {% endif %}{% endfor %}];
+    }{% if forloop.last %}{% else %}, {% endif %}{% endfor %}
+
+{% assign mascot_pages = site.pages | where: "layout", "mascot" %}
+{% for page in mascot_pages %}
+{
+  "id": {{ counter }},
+  "url": "{{ site.url }}{{ site.baseurl }}{{ page.url }}",
+  "title": "{{ page.title }}",
+  "body": "{{ page.content | markdownify | replace: '.', '. ' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"
+}{% assign counter = counter | plus: 1 %},{% endfor %}
+
+
+
+
+
+];
 
 var idx = lunr(function () {
     this.ref('id')
